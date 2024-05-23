5a9ae5ce59d061c71e7be6568fca4d6e57eed6ea
tonylapoche <terrade.antoine.pro@gmail.com>
2024-05-23 13:34:41 +0200

    ajout d'un log voir s'il fonctionne ce middleware u_u°

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,6 +1,7 @@
 import jwt from "jsonwebtoken";
 
 export default (req, res, next) => {
+  console.log("middleware/auth.js");
   try {
     const token = req.headers.authorization.split(" ")[1];
     const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
