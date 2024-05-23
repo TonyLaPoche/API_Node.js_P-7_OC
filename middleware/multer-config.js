5e9b9500f1e2c59888f42e7e5a56fcbfeaad832d
tonylapoche <terrade.antoine.pro@gmail.com>
2024-05-23 17:19:12 +0200

    chore: Add image upload functionality with multer middleware

--- middleware/multer-config.js
+++ middleware/multer-config.js
@@ -11,12 +11,12 @@ const storage = multer.diskStorage({
     callback(null, "images");
   },
   filename: (req, file, callback) => {
-    const name = file.originalname.split(" ").join("_").trim();
+    const name = file.originalname.split(" ").join("_");
     const extension = MIME_TYPES[file.mimetype];
     callback(null, name + Date.now() + "." + extension);
   },
 });
 
-const middlewareMulter = () => multer({ storage: storage }.single("image"));
+const upload = multer({ storage: storage });
 
-export default middlewareMulter;
+export default upload;
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ import { login, signup } from "../controller/auth/authenticate.js";
 import { createBook, deleteBook, getBestRatingBooks, getBook, getBooks, setBookRating, updateBook } from "../controller/books/books.js";
 import { clearAllBook, clearAllUser, getAllUser } from "../controller/admin/admin.js";
 import auth from "../middleware/auth.js";
-import middlewareMulter from "../middleware/multer-config.js";
+import upload from "../middleware/multer-config.js";
 
 const routes = Router();
 
@@ -15,7 +15,7 @@ routes.get("/books/:id", getBook);
 routes.get("/books/best", getBestRatingBooks);
 
 // middleware auth
-routes.post("/books", auth, middlewareMulter, createBook);
+routes.post("/books", auth, upload.single("image"), createBook);
 routes.put("/books/:id", auth, updateBook);
 routes.delete("/books/:id", auth, deleteBook);
 routes.put("/books/:id/rating", auth, setBookRating);
