90b7583ae512774d4a853804645df420b74afb65
tonylapoche <terrade.antoine.pro@gmail.com>
2024-05-24 09:45:36 +0200

    chore: Update book routes to include new functionality

--- routes/index.js
+++ routes/index.js
@@ -11,14 +11,14 @@ const routes = Router();
 routes.post("/auth/signup", signup);
 routes.post("/auth/login", login);
 routes.get("/books", getBooks);
+routes.get("/books/bestrating", getBestRatingBooks);
 routes.get("/books/:id", getBook);
-routes.get("/books/best", getBestRatingBooks);
 
 // middleware auth
-routes.post("/books", auth, upload.single("image"), createBook);
+routes.post("/books", auth, upload, createBook);
 routes.put("/books/:id", auth, updateBook);
 routes.delete("/books/:id", auth, deleteBook);
-routes.put("/books/:id/rating", auth, setBookRating);
+routes.post("/books/:id/rating", auth, setBookRating);
 
 // admin routes
 routes.get("/users", getAllUser);
