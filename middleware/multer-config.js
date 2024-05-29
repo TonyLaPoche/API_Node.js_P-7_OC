75282a4cb2e1839c36cef3f32a2ca5124a1b2ff9
tonylapoche <terrade.antoine.pro@gmail.com>
2024-05-29 16:57:29 +0200

    chore: Update multer-config.js to handle single image upload

--- middleware/multer-config.js
+++ middleware/multer-config.js
@@ -1,22 +1,7 @@
 import multer from "multer";
 
-const MIME_TYPES = {
-  "image/jpg": "jpg",
-  "image/jpeg": "jpg",
-  "image/png": "png",
-};
+const storage = multer.memoryStorage();
 
-const storage = multer.diskStorage({
-  destination: (req, file, callback) => {
-    callback(null, "images");
-  },
-  filename: (req, file, callback) => {
-    const name = file.originalname.split(" ").join("_");
-    const extension = MIME_TYPES[file.mimetype];
-    callback(null, name + Date.now() + "." + extension);
-  },
-});
-
-const upload = multer({ storage: storage }).single("image");
+const upload = multer({ storage: storage });
 
 export default upload;
