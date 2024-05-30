67ddb238b5cae165993e0f716cb2af2182478da2
tonylapoche <terrade.antoine.pro@gmail.com>
2024-05-30 13:42:56 +0200

    chore: Update multer-config.js to handle multiple image uploads

--- middleware/multer-config.js
+++ middleware/multer-config.js
@@ -1,7 +1,23 @@
 import multer from "multer";
 
+const MIME_TYPES = {
+  "image/jpg": "jpg",
+  "image/jpeg": "jpg",
+  "image/png": "png",
+  "image/gif": "gif",
+};
+
 const storage = multer.memoryStorage();
 
-const upload = multer({ storage: storage });
+const upload = multer({
+  storage: storage,
+  fileFilter: (req, file, cb) => {
+    if (MIME_TYPES[file.mimetype]) {
+      cb(null, true);
+    } else {
+      cb(new Error("Invalid file type"), false);
+    }
+  },
+});
 
 export default upload;
